bea07ded5c826cdda857f6f5df48a6b3275ce27c
Sajad Abasi <sab3053@gmail.com>
2019-03-02 19:12:46 +0330

    add some packages.

--- src/main.js
+++ src/main.js
@@ -8,16 +8,25 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/app.css'
 import BCardHeader from 'bootstrap-vue/es/components/card/card-header'
 import BCardBody from 'bootstrap-vue/es/components/card/card-body'
+import Element from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/fa'
+import 'element-ui/lib/theme-chalk/index.css';
+import ElMoment from 'el-moment'
+import HotelDatePicker from 'vue-hotel-datepicker'
 
 
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
-Vue.component('b-card-header', BCardHeader)
-Vue.component('b-card-body', BCardBody)
+Vue.use(ElMoment)
+Vue.use(Element , {locale});
+Vue.use(BootstrapVue);
+Vue.use(require('vue-moment-jalaali'));
+Vue.config.productionTip = false;
+Vue.component('b-card-header', BCardHeader);
+Vue.component('b-card-body', BCardBody);
+Vue.component('date-picker', HotelDatePicker);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
