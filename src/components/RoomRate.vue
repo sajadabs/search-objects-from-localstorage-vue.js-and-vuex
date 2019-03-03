2d5bb5bab3a2fceb93541d930b800f13a4710912
Sajad Abasi <sab3053@gmail.com>
2019-03-03 17:56:59 +0330

    define props and events and create days array.

--- src/components/RoomRate.vue
+++ src/components/RoomRate.vue
@@ -1,13 +1,14 @@
 <template>
     <div id="room-rate">
-        <Search></Search>
-        <Show></Show>
+        <Search @searchData="search($event)"></Search>
+        <Show :propId="propertyId" :day="day" v-for="(day , index) in days" :key="index"></Show>
     </div>
 </template>
 
 <script>
     import Search from './Search'
     import Show from './Show'
+    import moment from "moment-jalaali";
 
     export default {
         name: 'roomRate',
@@ -16,7 +17,33 @@
             Show
         },
         data() {
-            return {}
+            return {
+                propertyId: null,
+                date: {},
+                days: [],
+            }
+        },
+        methods: {
+            search(searchData) {
+                this.date.start = parseInt(searchData.startDate);
+                this.date.end = parseInt(searchData.endDate);
+                this.propertyId = searchData.propertyId;
+                this.days = [];
+                var date = this.date.start;
+                while (date <= this.date.end) {
+                    this.days.push(moment.unix(date).format("jYYYY/jMM/jDD"));
+                    date = date + 86400;
+                }
+            }
+        },
+        computed: {
+            roomTypes() {
+                return this.$store.getters.roomTypes
+            },
+            roomRates() {
+                return this.$store.getters.roomRates
+            }
         }
+
     }
 </script>
