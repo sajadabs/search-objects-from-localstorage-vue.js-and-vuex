d18aa4b8650b8b506fc21cc98ce5cb1050eb88fd
Sajad Abasi <sab3053@gmail.com>
2019-03-03 11:42:28 +0330

    add persian datepicker package.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './store/store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,22 +12,20 @@ import BCardBody from 'bootstrap-vue/es/components/card/card-body'
 import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/fa'
 import 'element-ui/lib/theme-chalk/index.css';
-import ElMoment from 'el-moment'
-import HotelDatePicker from 'vue-hotel-datepicker'
+import VuePersianDatetimePicker from 'vue-persian-datetime-picker';
 
-
-Vue.use(ElMoment)
-Vue.use(Element , {locale});
+Vue.component('date-picker', VuePersianDatetimePicker);
+Vue.use(Element, {locale});
 Vue.use(BootstrapVue);
 Vue.use(require('vue-moment-jalaali'));
 Vue.config.productionTip = false;
 Vue.component('b-card-header', BCardHeader);
 Vue.component('b-card-body', BCardBody);
-Vue.component('date-picker', HotelDatePicker);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    store,
+    components: {App},
+    template: '<App/>'
 });
